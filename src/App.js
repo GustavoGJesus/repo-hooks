ab1adce3cd683c0a1a77a06126efe631cc04f24c
GustavoGJesus <icarogabriel01@protonmail.com>
2021-07-08 04:25:42 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const  [location, setLocation] = useState({})
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(handlePositionReceived)
+  }, [])
+
+  function handlePositionReceived({coords}){
+    const { latitude, longitude } = coords
+    
+    setLocation ({ latitude, longitude})
+  }
+
+  useEffect(async() => {
+    const response = await fetch('https://api.github.com/users/GustavoGJesus/repos');
+    const data = await response.json();
+
+    setRepositories(data)
+  }, ['']);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    document.title = `Você tem ${filtered.length} favorito(s)`
+  }, [repositories]);
+
+  function handleFavorite (id){
+    const newRepositories =  repositories.map( repo => {
+      return repo.id === id ? {...repo, favorite: !repo.favorite } : repo
+    });
+
+    setRepositories(newRepositories)
+  }
+
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>{repo.name}
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorite(repo.id)}>
+            Favoritar
+          </button>
+          
+          </li>
+        ))}
+      </ul>
+      
+      
+      <p>Usando API de localização: </p>
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude}
+    </>
   );
 }
 
-export default App;
+
